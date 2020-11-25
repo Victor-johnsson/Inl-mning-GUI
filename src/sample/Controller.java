8addef12c5f1af16aed584b600e7677ded05be27
Victor-johnsson <70903378+Victor-johnsson@users.noreply.github.com>
2020-11-25 14:05:14 +0100

    added addPerson to controller!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,13 +4,43 @@ import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+
 import javafx.scene.*;
 
+import java.util.HashMap;
+
 public class Controller {
+
+	private PersonRegister personRegister = new PersonRegister();
+	@FXML private TextField pNbr_textField;
 	@FXML private TextField name_textField;
-	public void pressButton(ActionEvent event){
-		System.out.println(name_textField.getText());
-
-
+	public Controller(){
+		HashMap<String, Person> register = new HashMap<>();
+		personRegister.setPersonHashMap(register);
 	}
+
+	public void addPerson(ActionEvent event){
+		String name = name_textField.getText();
+		String pNbr = pNbr_textField.getText();
+		try{
+			if(pNbr.isEmpty() && name.isEmpty()){
+				System.out.println("Name and pnbr is empty!");
+			}else if(pNbr.isEmpty()) {
+				System.out.println("pNbr is empty");
+			}else if(name.isEmpty()){
+				System.out.println("Name is empty!");
+			}else{
+				Person tmpPerson = new Person();
+				tmpPerson.setName(name);
+				tmpPerson.setpNbr(pNbr);
+				personRegister.addPerson(tmpPerson);
+				System.out.println("Person " + personRegister.findPerson(pNbr).getName() + " was added to list! ");
+			}
+		}catch (Exception e1){
+			System.out.println("Error: " + e1);
+		}
+	}
+
+
+
 }
