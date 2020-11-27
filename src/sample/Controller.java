6c9892e5abd01fc43d6c51557406676b3844df6f
Victor-johnsson <70903378+Victor-johnsson@users.noreply.github.com>
2020-11-27 15:08:58 +0100

    Made window undecorated, added exit button and made it so you can move the window around!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,56 +1,73 @@
 package sample;
+
 import javafx.event.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.util.HashMap;
 
 import static java.lang.StrictMath.abs;
 
 public class Controller {
-	private final PersonRegister personRegister = new PersonRegister();
-	private final HashMap<String,Account> allAccount;
-	@FXML private TextField pNbr_textField;
-	@FXML private TextField name_textField;
-	@FXML private TextField accountNbr_textField;
-	@FXML private TextArea mainTxtArea;
-	@FXML private RadioButton creditRadioBtn;
-	@FXML private RadioButton withdrawRadioBtn;
-	@FXML private TextField amount_textField;
 
-	public Controller(){
+	private final PersonRegister personRegister = new PersonRegister();
+	private final HashMap<String, Account> allAccount;
+	@FXML
+	private TextField pNbr_textField;
+	@FXML
+	private TextField name_textField;
+	@FXML
+	private TextField accountNbr_textField;
+	@FXML
+	private TextArea mainTxtArea;
+	@FXML
+	private RadioButton creditRadioBtn;
+	@FXML
+	private RadioButton withdrawRadioBtn;
+	@FXML
+	private TextField amount_textField;
+	@FXML
+	public Button close_menu;
+
+
+	public Controller() {
 		HashMap<String, Person> register = new HashMap<>();
 		personRegister.setPersonHashMap(register);
-		this.allAccount= new HashMap<>();
+		this.allAccount = new HashMap<>();
 	}
 
-	public void addPerson(ActionEvent event){
+	@FXML
+	public void handleCloseButtonAction(ActionEvent event) {
+		Stage stage = (Stage) close_menu.getScene().getWindow();
+		stage.close();
+	}
+
+	public void addPerson(ActionEvent event) {
 		String name = name_textField.getText();
 		String pNbr = pNbr_textField.getText();
 		/* Testar så att inga nödvändiga fält är tomma.
 		  Ifall de är tomma kommer metoden att avbrytas och skriva ut ett meddelande som användaren kan se!
 		  Ifall allt gått rätt till skapas en ny person som läggs till i ett PersonRegister!
 		 */
-		try{
-			if(pNbr.isEmpty() || name.isEmpty()){
+		try {
+			if (pNbr.isEmpty() || name.isEmpty()) {
 				mainTxtArea.setText("Name or Personal ID is empty!");
-			}else if(personRegister.findPerson(pNbr)!= null){
-					mainTxtArea.setText("There already exist a person with this personal ID!");
-			}else{
+			} else if (personRegister.findPerson(pNbr) != null) {
+				mainTxtArea.setText("There already exist a person with this personal ID!");
+			} else {
 				Person tmpPerson = new Person();
 				tmpPerson.setName(name);
 				tmpPerson.setpNbr(pNbr);
 				personRegister.addPerson(tmpPerson);
 				mainTxtArea.setText(personRegister.findPerson(pNbr).getName() + " was added to list!");
 			}
-		}catch (Exception e1){
+		} catch (Exception e1) {
 			System.out.println("Error: " + e1);
 		}
 	}
 
-	public void addAccount(ActionEvent event){
+	public void addAccount(ActionEvent event) {
 		String pNbr = pNbr_textField.getText();
 		String accountNbr = accountNbr_textField.getText();
 		/* Testar så att inga nödvändiga fält är tomma.
@@ -60,23 +77,23 @@ public class Controller {
 
 		  Även om detta skulle kunna vara lugnt då ett konto endast går att komma åt från en person tolkade vi det som att ett konto endast kan tillhöra en person!
 		 */
-		try{
-			if(pNbr.isEmpty() || accountNbr.isEmpty()){
+		try {
+			if (pNbr.isEmpty() || accountNbr.isEmpty()) {
 				mainTxtArea.setText("Personal ID or Account Number is empty!");
-			}else if(personRegister.findPerson(pNbr)==null){
+			} else if (personRegister.findPerson(pNbr) == null) {
 				mainTxtArea.setText("Could not find a person with this personal ID!");
-			}else if(allAccount.containsKey(accountNbr)){
+			} else if (allAccount.containsKey(accountNbr)) {
 				mainTxtArea.setText("Account already exists!");
-			}else{
+			} else {
 				Account tmpAccount = new Account();
 				tmpAccount.setAccountNbr(accountNbr);
 				tmpAccount.setOwner(personRegister.findPerson(pNbr));
 				personRegister.findPerson(pNbr).addAccount(tmpAccount);
-				allAccount.put(tmpAccount.getAccountNbr(),tmpAccount);
-				mainTxtArea.setText("Account: " + "'" +personRegister.findAccount(pNbr,accountNbr).getAccountNbr() + "'"+
-						" was added to: " + personRegister.findAccount(pNbr,accountNbr).getOwner().getName() + "'s accounts!");
+				allAccount.put(tmpAccount.getAccountNbr(), tmpAccount);
+				mainTxtArea.setText("Account: " + "'" + personRegister.findAccount(pNbr, accountNbr).getAccountNbr() + "'" +
+						" was added to: " + personRegister.findAccount(pNbr, accountNbr).getOwner().getName() + "'s accounts!");
 			}
-		}catch (Exception e1){
+		} catch (Exception e1) {
 			System.out.println("Error: " + e1);
 		}
 	}
@@ -86,25 +103,25 @@ public class Controller {
 		String accountNbr = accountNbr_textField.getText();
 		String personalNbr = pNbr_textField.getText();
 		double amount = 0;
-		if(!(amount_textField.getText().isEmpty())){
+		if (!(amount_textField.getText().isEmpty())) {
 			amount = abs(Double.valueOf(amount_textField.getText()));
 		}
 		try {
-			if (accountNbr.isEmpty() || personalNbr.isEmpty() || amount_textField.getText().isEmpty() ) {
+			if (accountNbr.isEmpty() || personalNbr.isEmpty() || amount_textField.getText().isEmpty()) {
 				mainTxtArea.setText("One of the required fields is empty!");
 			} else if (personRegister.findPerson(personalNbr) == null) {
 				mainTxtArea.setText("Could not find a person with this personal ID!");
 			} else if (personRegister.findAccount(personalNbr, accountNbr) == null) {
 				mainTxtArea.setText("Person doesn't own an account with this number!");
-			} else if(creditRadioBtn.isSelected()){
+			} else if (creditRadioBtn.isSelected()) {
 				personRegister.findAccount(personalNbr, accountNbr).credit(amount);
 				mainTxtArea.setText("Credited " + amount + " to account '"
-						+ personRegister.findAccount(personalNbr,accountNbr).getAccountNbr() + "'" + "\n"+
+						+ personRegister.findAccount(personalNbr, accountNbr).getAccountNbr() + "'" + "\n" +
 						"New balance is: " + personRegister.findAccount(personalNbr, accountNbr).getBalance());
-			}else if(withdrawRadioBtn.isSelected()) {
+			} else if (withdrawRadioBtn.isSelected()) {
 				personRegister.findAccount(personalNbr, accountNbr).withdraw(amount);
 				mainTxtArea.setText("Withdrew " + amount + " from account '"
-						+ personRegister.findAccount(personalNbr, accountNbr).getAccountNbr() + "'" + "\n"+
+						+ personRegister.findAccount(personalNbr, accountNbr).getAccountNbr() + "'" + "\n" +
 						"New balance is: " + personRegister.findAccount(personalNbr, accountNbr).getBalance());
 			}
 		} catch (Exception e1) {
@@ -112,25 +129,26 @@ public class Controller {
 		}
 	}
 
-	public void showButton(ActionEvent event){
+	public void showButton(ActionEvent event) {
 		String pNbr = pNbr_textField.getText();
-		try{
-			if(pNbr.isEmpty()){
+		try {
+			if (pNbr.isEmpty()) {
 				mainTxtArea.setText("Personal ID is empty!");
-			}else if(personRegister.findPerson(pNbr) == null){
+			} else if (personRegister.findPerson(pNbr) == null) {
 				mainTxtArea.setText("Could not find a person with this personal ID!");
-			}else{
+			} else {
 				String personName = personRegister.findPerson(pNbr).getName();
-				String allAccount ="";
+				String allAccount = "";
 				for (Account tmpAccount : personRegister.findPerson(pNbr).getAccounts().values()) {
 					allAccount += "Account: " + tmpAccount.getAccountNbr() + " has balance: " + tmpAccount.getBalance() + "\n";
 				}
 				mainTxtArea.setText(personName + "'s accounts: " + "\n" + allAccount + "Total balance: " + personRegister.findPerson(pNbr).totBalance());
 			}
-		}catch (Exception e1){
+		} catch (Exception e1) {
 			System.out.println("Error: " + e1);
 		}
 	}
+
 	public void removePerson(ActionEvent event) {
 		String pNbr = pNbr_textField.getText();
 		try {
@@ -140,7 +158,7 @@ public class Controller {
 				mainTxtArea.setText("Could not find a person with this personal ID!");
 			} else {
 				Person tmp = personRegister.findPerson(pNbr);
-				for (Account a:tmp.getAccounts().values()) {
+				for (Account a : tmp.getAccounts().values()) {
 					allAccount.remove(a.getAccountNbr(), a);
 				}
 				personRegister.removePerson(pNbr);
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,22 +2,55 @@ package sample;
 
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+import java.util.logging.Handler;
 
 public class Main extends Application {
-
-
+    Stage window;
+    private double xOffset=0;
+    private double yOffset=0;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Register");
-        primaryStage.setScene(new Scene(root, 700, 380));
-        primaryStage.show();
+        window=primaryStage;
+        window.setTitle("Register");
+        window.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                xOffset = window.getX() - mouseEvent.getScreenX();
+                yOffset= window.getY() - mouseEvent.getScreenY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                window.setX(mouseEvent.getScreenX() + xOffset);
+                window.setY(mouseEvent.getScreenY() + yOffset);
+            }
+        });
+        window.setScene(new Scene(root, 700, 380));
+
+        window.show();
     }
+
+
     public static void main(String[] args) {
         launch(args);
     }
